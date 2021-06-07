2c26212046038825d267d0e7fbd26007d60f385b
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-06-07 10:12:53 -0300

    Solving bugs

--- ifrj/avaliação 3/exercicio04.py
+++ ifrj/avaliação 3/exercicio04.py
@@ -1,11 +1,15 @@
 mercado = []
-for i in range(70):
+for i in range(3):
     codigo = input('Código: ')
     preco = float(input('Preço: '))
     quantidade = int(input('Quantidade: '))
     mercado.append([codigo, preco, quantidade])
 
 pagamento = 0
+print('-'*50)
+print('#'*50)
+print('-'*50)
+print('Agora insira o código do produto a ser comprado: ')
 while True:
     text = input('Código: ')
     if text == '-1':
@@ -17,11 +21,12 @@ while True:
             if item[0] == codigo:
                 tester = True
                 compra = int(input('Quantidade: '))
-                if compra > item[1]:
+                if compra > item[2]:
                     print(f'Estoque insuficiente, tem apenas {item[2]} em estoque')
                 else:
                     mercado.insert(mercado.index(item), [item[0], item[1], item[2] - compra])
                     pagamento += compra * item[1]
+                    break
         if not tester:
             print('Erro, Código inválido')
 
