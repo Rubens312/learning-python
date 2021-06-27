7f6427b87d3272952dc401e4580962577f609d82
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-06-27 11:19:34 -0300

    Jogo da velha funcionando

--- tic tac toe/tictactoe.py
+++ tic tac toe/tictactoe.py
@@ -1,8 +1,8 @@
 board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
 def print_board(board):
-    for i in range(3):
-        for j in range(3):
-            print(board[i][j], end=' ')
+    for line in board:
+        for position in line:
+            print(position, end=' ')
         print()
     print()
 
@@ -19,22 +19,26 @@ def tie_test(board):
 def play_game(board):
     while True:
         print_board(board)
-        line = int(input('Selecione uma linha: '))
-        column = int(input('Selecione uma coluna: '))
-        board[line - 1].insert(column - 1, 'X')
-        if board[line - 1] == ['X', 'X','X'] or (board[0][column] == 'X' and board[1][column] == 'X' and board[2][column] == 'X') or (board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X') or (board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X'):
+        line = int(input('Selecione uma linha: ')) - 1
+        column = int(input('Selecione uma coluna: ')) - 1
+        board[line][column] = 'X'
+        if board[line] == ['X', 'X','X'] or (board[0][column] == 'X' and board[1][column] == 'X' and board[2][column] == 'X') or (board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X') or (board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X'):
+            print_board(board)
             print('X venceu')
             break
         else:
             if tie_test:
                 print_board(board)
-                line = int(input('Selecione uma linha: '))
-                column = int(input('Selecione uma coluna: '))
-                board[line - 1].insert(column - 1, 'O')
-                if board[line - 1] == ['O', 'O','O'] or (board[0][column] == 'O' and board[1][column] == 'O' and board[2][column] == 'O') or (board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O') or (board[0][2] == 'O' and board[1][1] == 'O' and board[2][0] == 'O'):
+                line = int(input('Selecione uma linha: ')) - 1
+                column = int(input('Selecione uma coluna: ')) - 1
+                board[line][column] = 'O'
+                if board[line] == ['O', 'O','O'] or (board[0][column] == 'O' and board[1][column] == 'O' and board[2][column] == 'O') or (board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O') or (board[0][2] == 'O' and board[1][1] == 'O' and board[2][0] == 'O'):
+                    print_board(board)
                     print('O venceu')
                     break
             else:
+                print_board(board)
                 print('Velha')
-                break        
+                break
+                    
 play_game(board)
