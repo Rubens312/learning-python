b7dd0d00ace8b29773f769c9340c834f3a3eff2b
Rubens Kaiserman <rubskaiserman@gmail.com>
2021-08-25 18:33:24 -0300

    Só brincando um pouco

---
+++ extras/trip.py
@@ -0,0 +1,10 @@
+class HelloWorld:
+    def __init__(self, string):
+        self.hello_world = string
+    
+    def hello_World(self):
+        print(self.hello_world)
+    
+ola_mundo = HelloWorld('batata')
+
+ola_mundo.hello_World()
